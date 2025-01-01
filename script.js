1e96c17ca62d64fbcc01b1005bbac08c20a9beaf
Pankaj  Parmar <115024744+Viratparmar913@users.noreply.github.com>
2025-01-01 18:38:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ const mainEl = $("#main");
 // Default coordinates for initial load (Delhi)
 let lat = "28.666668";
 let lon = "77.216667";
+let apikey = apiKey;
 //let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&exclude=hourly`;
-let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=alerts&appid=${apiKey}`;
+let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=alerts&appid=${apikey}`;
 
 let cityName = "Delhi";
 let firstLoad = true;
